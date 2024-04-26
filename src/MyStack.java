41f71bd2fe3271062720812c22cc7c6bf1998f10
DayanaNur <154298960+DayanaNur@users.noreply.github.com>
2024-04-26 11:50:16 +0500

    commit

---
+++ src/MyStack.java
@@ -0,0 +1,34 @@
+public class MyStack<T> {
+    private MyArrayList<T> list;
+
+    public MyStack() {
+        list = new MyArrayList<T>() {
+
+            public void push(T item) {
+                list.add(item);
+            }
+
+            public T pop() {
+                if (isEmpty()) {
+                    throw new IllegalStateException("Stack is empty");
+                }
+                return list.remove(list.size() - 1);
+            }
+
+            public T peek() {
+                if (isEmpty()) {
+                    throw new IllegalStateException("Stack is empty");
+                }
+                return list.get(list.size() - 1);
+            }
+
+            public boolean isEmpty() {
+                return list.isEmpty();
+            }
+
+            public int size() {
+                return list.size();
+            }
+        };
+    }
+}
