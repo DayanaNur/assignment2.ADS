a4a829a8243eeb80b01031d2649c05201fef66e4
DayanaNur <154298960+DayanaNur@users.noreply.github.com>
2024-04-26 11:45:11 +0500

    change the structure , add "does list contain 25"

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayList<Integer> myList = new MyArrayList<>();
-        Integer[] MyArrayList = new Integer[0];
-        myList.add(3);
-        myList.addFirst(0);
-        myList.addLast(1);
-
-        for (Integer item : MyArrayList) {
-            System.out.print(item + " ");
-        }
-        System.out.println("First element: " + myList.getFirst());
-        System.out.println("Last element: " + myList.getLast());
-        System.out.println("Element at index 1: " + myList.get(1));
-        myList.set(1, 5);
-        System.out.println("Element at index 1 after setting: " + myList.get(1));
-        myList.remove(1);
-        myList.removeFirst();
-        myList.removeLast();
-        System.out.println("Size after removals: " + myList.size());
-        myList.add(5);
-        myList.add(3);
-        myList.add(1);
-        System.out.println();
-        myList.sort();
-        for (Integer item : MyArrayList) {
-            System.out.print(item + " ");
-        }
-        System.out.println();
-        myList.add(3);
-        System.out.println("Index of 3: " + myList.indexOf(3));
-        System.out.println("Last index of 3: " + myList.lastIndexOf(3));
-        Object[] array = myList.toArray();
-        System.out.print("Array contents: ");
-        for (Object item : array) {
-            System.out.print(item + " ");
+        MyArrayList<Integer> MyList = new MyArrayList<>() {
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        };
+        MyList.add(10);
+        MyList.add(20);
+        MyList.add(30);
+        MyList.add(1, 15);
+        MyList.set(2, 25);
+        System.out.println("Element at index 2: " + MyList.get(2));
+        MyList.addFirst(5);
+        MyList.addLast(35);
+        MyList.remove(3);
+        MyList.removeFirst();
+        MyList.removeLast();
+        System.out.println("Size of list: " + MyList.size());
+        System.out.println("Does list contain 25?: " + MyList.exists(25));
+        System.out.println("Index of element 20: " + MyList.indexOf(20));
+        Object[] array = MyList.toArray();
+        System.out.print("Size of array list: ");
+        for (Object element : array) {
+            System.out.print(element + " ");
         }
         System.out.println();
-        myList.clear();
-        System.out.println("Size after clearing: " + myList.size()); // Output: 0
+        MyList.clear();
+        System.out.println("Size of list after clear: " + MyList.size());
     }
 }
